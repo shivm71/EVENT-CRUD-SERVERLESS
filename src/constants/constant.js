f59038e8d14f8de8334f346b44be61256ed8753c
Shivam Shah <shivamshah71@gmail.com.com>
2020-06-29 20:41:48 +0530

    initial frontend

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+import { Auth } from 'aws-amplify';
+
+export const  apiName = 'noreqapi';
+export const  path    = '/events';
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App1 from './App1';
+
+import * as serviceWorker from './serviceWorker';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+import Mod from "./Mod";
+Amplify.configure(awsExports);
+
+ReactDOM.render(
+ 
+  <App1/>,
+ 
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
